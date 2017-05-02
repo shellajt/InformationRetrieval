46856ed6a52f04b744282374471e20b2f39b4e08
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-05-02 19:27:44 -0400

    display top five results and lowercase the input

--- src/main.java
+++ src/main.java
@@ -5,8 +5,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class main {
 
@@ -18,6 +17,7 @@ public class main {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             try {
                 String searchQuery = br.readLine();
+                searchQuery = searchQuery.toLowerCase();
                 if (searchQuery.equals("exit")) exit();
                 irSearch(directory, searchQuery);
             } catch (IOException e) {
@@ -52,17 +52,35 @@ public class main {
             finalScores.put(filename, score);
         }
 
-        //Find the max and print it out
-        Double maxScore = 0.0;
-        String maxFilename = "Error!";
-        for (String filename : finalScores.keySet()) {
-            Double score = finalScores.get(filename);
-            if (score > maxScore) {
-                maxScore = score;
-                maxFilename = filename.split(" ")[0];
+        System.out.println(getTopFiveResults(finalScores));
+    }
+
+    private static String getTopFiveResults(Map<String, Double> finalScores) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("We suggest the following five documents, sorted in order of their relevance.");
+        sb.append("\r\n\t");
+
+        List<String> usedFiles = new LinkedList();
+
+        for (int i = 0; i < 5; i++) {
+            Double maxScore = -1.0;
+            String maxFilename = "Error!";
+            for (String filename : finalScores.keySet()) {
+                Double score = finalScores.get(filename);
+                if (score > maxScore && !usedFiles.contains(filename.split(" ")[0])) {
+                    maxScore = score;
+                    maxFilename = filename.split(" ")[0];
+                }
             }
+            if (maxFilename.equals("Error!")) return sb.toString();
+            usedFiles.add(maxFilename);
+
+            sb.append(maxFilename);
+            sb.append(".txt with a score of ");
+            sb.append(String.format("%.3f", maxScore));
+            sb.append("\r\n\t");
         }
-        String output = String.format("We suggest the file %s.txt. Our system gave it a score of %.3f.", maxFilename, maxScore);
-        System.out.println(output);
+        System.out.println(usedFiles.toString());
+        return sb.toString();
     }
 }
